2bc6a48338f6e3f6d0e3087265c4c4cd7a9c1bb0
tom_glb <goodMorning_glb@atguigu.com>
2020-07-28 22:42:36 +0800

    负载均衡、actuator微服务信息完善

--- cloud-provider-payment8001/src/main/java/com/domore/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/domore/springcloud/controller/PaymentController.java
@@ -4,9 +4,13 @@ import com.domore.springcloud.entities.CommonResult;
 import com.domore.springcloud.entities.Payment;
 import com.domore.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -20,13 +24,19 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("*****插入结果：{}.", result);
 
         if (result > 0) {
-            return new CommonResult(200, "插入数据成功", result);
+            return new CommonResult(200, "插入数据成功，serverPort：" + serverPort, result);
         } else {
             return new CommonResult(444, "插入数据失败");
         }
@@ -38,9 +48,20 @@ public class PaymentController {
         log.info("*****查询结果：{}.", payment);
 
         if (Objects.nonNull(payment)) {
-            return new CommonResult(200, "查询成功", payment);
+            return new CommonResult(200, "查询成功，serverPort：" + serverPort, payment);
         } else {
             return new CommonResult(444, String.format("记录不存在，id：%s", id));
         }
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        services.forEach(service -> log.info("*****service: {}.", service));
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        instances.forEach(instance -> log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri()));
+
+        return this.discoveryClient;
     }
 }
