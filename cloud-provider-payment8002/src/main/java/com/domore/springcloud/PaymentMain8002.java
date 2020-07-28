839b650d6f255d8f1028bf1e0d154b370fdcf57c
tom_glb <goodMorning_glb@atguigu.com>
2020-07-28 22:34:45 +0800

    支付服务提供集群环境，8002

---
+++ cloud-provider-payment8002/src/main/java/com/domore/springcloud/PaymentMain8002.java
@@ -0,0 +1,18 @@
+package com.domore.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @FileName: PaymentMain8001
+ * @Author: zhao
+ * @Date: 2020/7/21 19:18
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentMain8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8002.class, args);
+    }
+}
---
+++ cloud-provider-payment8002/src/main/java/com/domore/springcloud/controller/PaymentController.java
@@ -0,0 +1,50 @@
+package com.domore.springcloud.controller;
+
+import com.domore.springcloud.entities.CommonResult;
+import com.domore.springcloud.entities.Payment;
+import com.domore.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.Objects;
+
+/**
+ * @FileName: PaymentController
+ * @Author: zhao
+ * @Date: 2020/7/22 19:53
+ **/
+@RestController
+@Slf4j
+public class PaymentController {
+    @Resource
+    private PaymentService paymentService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(@RequestBody Payment payment) {
+        int result = paymentService.create(payment);
+        log.info("*****插入结果：{}.", result);
+
+        if (result > 0) {
+            return new CommonResult(200, "插入数据成功，serverPort：" + serverPort, result);
+        } else {
+            return new CommonResult(444, "插入数据失败");
+        }
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("*****查询结果：{}.", payment);
+
+        if (Objects.nonNull(payment)) {
+            return new CommonResult(200, "查询成功，serverPort：" + serverPort, payment);
+        } else {
+            return new CommonResult(444, String.format("记录不存在，id：%s", id));
+        }
+    }
+}
